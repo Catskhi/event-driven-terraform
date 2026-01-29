3fe53a80ef064ad8bb1d811de566001734bd6257
catskhi <catskhi@proton.me>
2026-01-29 15:12:26 -0300

    feat: update Lambda function to use dynamic filename and source code hash from archive

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -3,7 +3,8 @@ resource "aws_lambda_function" "lambda_function" {
     role         = aws_iam_role.lambda-role.arn
     handler       = "index.handler"
     runtime      = "nodejs20.x"
-    filename = "lambda.zip"
+    filename = data.archive_file.lambda.output_path
+    source_code_hash = data.archive_file.lambda.output_base64sha256
     environment {
         variables = {
             TABLE_NAME = aws_dynamodb_table.tasks.name
