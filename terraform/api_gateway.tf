e3757d8ae02f4a0cfec20095156dfa56c942a753
catskhi <catskhi@proton.me>
2026-01-29 13:42:34 -0300

    feat: add API Gateway configuration with Cognito authorizer

---
+++ terraform/api_gateway.tf
@@ -0,0 +1,48 @@
+resource "aws_apigatewayv2_api" "main" {
+    name         = "${var.project_name}-${var.environment}-api"
+    protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_integration" "lambda" {
+    api_id = aws_apigatewayv2_api.main.id
+    integration_type = "AWS_PROXY"
+    integration_uri = aws_lambda_function.lambda_function.invoke_arn
+    integration_method = "POST"
+    payload_format_version = "2.0"
+}
+
+resource "aws_apigatewayv2_route" "default" {
+    api_id = aws_apigatewayv2_api.main.id
+    route_key = "ANY /{proxy+}"
+    target = "integrations/${aws_apigatewayv2_integration.lambda.id}"
+
+    authorization_type = "JWT"
+    authorizer_id = aws_apigatewayv2_authorizer.cognito.id
+}
+
+resource "aws_apigatewayv2_stage" "default" {
+    api_id = aws_apigatewayv2_api.main.id
+    name = var.environment
+    auto_deploy = true
+}
+
+resource "aws_lambda_permission" "api_gateway" {
+    statement_id = "AllowAPIGateway"
+    action       = "lambda:InvokeFunction"
+    function_name = aws_lambda_function.lambda_function.function_name
+    principal = "apigateway.amazonaws.com"
+    source_arn = "${aws_apigatewayv2_api.main.execution_arn}/*/*"
+}
+
+
+resource "aws_apigatewayv2_authorizer" "cognito" {
+  api_id = aws_apigatewayv2_api.main.id
+  authorizer_type = "JWT"
+  name = "cognito-authorizer"
+  identity_sources = ["$request.header.Authorization"]
+
+  jwt_configuration {
+    issuer = "https://cognito-idp.${var.aws_region}.amazonaws.com/${aws_cognito_user_pool.main.id}"  
+    audience = [aws_cognito_user_pool_client.main.id]
+  }
+}
